c675405f0e9ad0560d1f8ec4978a241a88071637
vidurkhanal <vkhanal@caldwell.edu>
2023-04-06 21:00:27 -0400

    feat: lexar completed

--- src/lexer.rs
+++ src/lexer.rs
@@ -31,11 +31,29 @@ impl Lexer {
         self.read_position += 1;
     }
 
+    fn peek_char(&mut self) -> u8 {
+        if self.read_position >= self.input.len() {
+            0
+        } else {
+            self.input.as_bytes()[self.read_position]
+        }
+    }
+
     fn next_token(&mut self) -> Token {
         self.skip_whitespace();
 
         let next_tok = match self.ch {
-            b'=' => Token::new(TokenKind::ASSIGN, String::from(self.ch as char)),
+            b'=' => {
+                if self.peek_char() == b'=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    let mut literal = String::from(ch as char);
+                    literal.push(self.ch as char);
+                    Token::new(TokenKind::EQ, literal)
+                } else {
+                    Token::new(TokenKind::ASSIGN, String::from(self.ch as char))
+                }
+            }
             b';' => Token::new(TokenKind::SEMICOLON, String::from(self.ch as char)),
             b'(' => Token::new(TokenKind::LPAREN, String::from(self.ch as char)),
             b')' => Token::new(TokenKind::RPAREN, String::from(self.ch as char)),
@@ -43,6 +61,22 @@ impl Lexer {
             b'+' => Token::new(TokenKind::PLUS, String::from(self.ch as char)),
             b'{' => Token::new(TokenKind::LBRACE, String::from(self.ch as char)),
             b'}' => Token::new(TokenKind::RBRACE, String::from(self.ch as char)),
+            b'-' => Token::new(TokenKind::MINUS, String::from(self.ch as char)),
+            b'!' => {
+                if self.peek_char() == b'=' {
+                    let ch = self.ch;
+                    self.read_char();
+                    let mut literal = String::from(ch as char);
+                    literal.push(self.ch as char);
+                    Token::new(TokenKind::NEQ, literal)
+                } else {
+                    Token::new(TokenKind::BANG, String::from(self.ch as char))
+                }
+            }
+            b'*' => Token::new(TokenKind::ASTERISK, String::from(self.ch as char)),
+            b'/' => Token::new(TokenKind::SLASH, String::from(self.ch as char)),
+            b'>' => Token::new(TokenKind::GT, String::from(self.ch as char)),
+            b'<' => Token::new(TokenKind::LT, String::from(self.ch as char)),
             _ => {
                 if Self::is_letter(self.ch) {
                     let ident = self.read_identifier();
@@ -177,6 +211,86 @@ mod lexer_test {
             (TokenKind::RPAREN, ")"),
             (TokenKind::SEMICOLON, ";"),
             (TokenKind::EOF, " "),
+        ];
+        let mut l = Lexer::new(input);
+        for (i, tt) in tests.iter().enumerate() {
+            let tok = l.next_token();
+            assert_eq!(
+                tok.kind, tt.0,
+                "tests[{0}] - tokentype wrong. expected={1:?}, got={2:?}",
+                i, tt.0, tok.kind
+            );
+            assert_eq!(
+                tok.literal, tt.1,
+                "tests[{}] - literal wrong. expected={}, got={}",
+                i, tt.1, tok.literal
+            );
+        }
+    }
+
+    #[test]
+    fn test_next_token3() {
+        let input = "if x > 4{
+            return true;
+        };";
+        let tests = vec![
+            (TokenKind::IF, "if"),
+            (TokenKind::IDENT, "x"),
+            (TokenKind::GT, ">"),
+            (TokenKind::INT, "4"),
+            (TokenKind::LBRACE, "{"),
+            (TokenKind::RETURN, "return"),
+            (TokenKind::TRUE, "true"),
+            (TokenKind::SEMICOLON, ";"),
+            (TokenKind::RBRACE, "}"),
+            (TokenKind::SEMICOLON, ";"),
+        ];
+        let mut l = Lexer::new(input);
+        for (i, tt) in tests.iter().enumerate() {
+            let tok = l.next_token();
+            assert_eq!(
+                tok.kind, tt.0,
+                "tests[{0}] - tokentype wrong. expected={1:?}, got={2:?}",
+                i, tt.0, tok.kind
+            );
+            assert_eq!(
+                tok.literal, tt.1,
+                "tests[{}] - literal wrong. expected={}, got={}",
+                i, tt.1, tok.literal
+            );
+        }
+    }
+
+    #[test]
+    fn test_next_token4() {
+        let input = "
+            !-/*5;
+            5 < 10 > 5;
+
+            10 == 10;
+            10!=9;
+        ";
+        let tests = vec![
+            (TokenKind::BANG, "!"),
+            (TokenKind::MINUS, "-"),
+            (TokenKind::SLASH, "/"),
+            (TokenKind::ASTERISK, "*"),
+            (TokenKind::INT, "5"),
+            (TokenKind::SEMICOLON, ";"),
+            (TokenKind::INT, "5"),
+            (TokenKind::LT, "<"),
+            (TokenKind::INT, "10"),
+            (TokenKind::GT, ">"),
+            (TokenKind::INT, "5"),
+            (TokenKind::SEMICOLON, ";"),
+            (TokenKind::INT, "10"),
+            (TokenKind::EQ, "=="),
+            (TokenKind::INT, "10"),
+            (TokenKind::SEMICOLON, ";"),
+            (TokenKind::INT, "10"),
+            (TokenKind::NEQ, "!="),
+            (TokenKind::INT, "9"),
+            (TokenKind::SEMICOLON, ";"),
         ];
         let mut l = Lexer::new(input);
         for (i, tt) in tests.iter().enumerate() {
--- src/token/keywords.rs
+++ src/token/keywords.rs
@@ -3,6 +3,11 @@ use super::token_kind::TokenKind;
 static KEYWORDS_MAP: phf::Map<&'static str, TokenKind> = phf_map! {
     "func" => TokenKind::FUNCTION,
     "let" => TokenKind::LET,
+    "true" => TokenKind::TRUE,
+    "false" => TokenKind::FALSE,
+    "if" => TokenKind::IF,
+    "else" => TokenKind::ELSE,
+    "return" => TokenKind::RETURN,
 };
 
 pub fn lookup_ident(ident: &str) -> TokenKind {
